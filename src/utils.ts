665bff96dd756556517aaa753a94621383a9d172
idrissar <ridwan.idris_sabi@contractors.roche.com>
2022-12-15 09:34:03 +0800

    fix nanoid issue;

--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,4 @@
-// import { nanoid } from 'nanoid';
+import { nanoid } from 'nanoid';
 
 export const validateUrl = (url: string): boolean => {
   return /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+(:[0-9]+)?|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))?)/.test(
@@ -6,7 +6,6 @@ export const validateUrl = (url: string): boolean => {
   );
 };
 
-export const generateId = (): string => {
-  // return nanoid(5);
-  return Date.now().toString();
+export const generateId = (size?: number): string => {
+  return nanoid(size || 6);
 };
